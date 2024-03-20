e4e69483fa205755258950d989d4eb45bb022b2f
ashnchiquita <16521248@mahasiswa.itb.ac.id>
2024-03-20 09:14:43 +0700

    feat: default vertex color

--- src/primitives/Vertex.ts
+++ src/primitives/Vertex.ts
@@ -2,8 +2,8 @@ export default class Vertex {
   public coord: number[]
   public color: number[]
 
-  constructor (coord: number[], color: number[]) {
+  constructor (coord: number[], color?: number[]) {
     this.coord = coord
-    this.color = color
+    this.color = color ?? [0, 0, 0, 1]
   }
 }
